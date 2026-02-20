885ad31383c64ba64a004e7cc0db4c79a86936fa
Erik Ejlskov Jensen <eej@delegate.dk>
2026-02-20 14:33:43 +0100

    more demos

---
+++ src/IsoWeek/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+Console.WriteLine(ISOWeek.GetWeekOfYear(new DateOnly(2024, 12, 31)));
+
+Console.WriteLine(ISOWeek.GetWeekOfYear(new DateOnly(2025, 12, 31)));
+
+Console.WriteLine(ISOWeek.GetWeekOfYear(new DateOnly(2026, 12, 31)));
+
+Console.WriteLine(ISOWeek.GetYear(new DateOnly(2024, 1, 1)));
---
+++ src/StringCompare/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+StringComparer numericStringComparer = StringComparer.Create(CultureInfo.CurrentCulture, CompareOptions.StringSort);
+
+Console.WriteLine(numericStringComparer.Equals("02", "2"));
+
+foreach (string dotnet in new[] { ".NET 8", ".NET 10", ".NET 11" }.Order(numericStringComparer))
+{
+    Console.WriteLine(dotnet);
+}
+
+HashSet<string> set = new HashSet<string>(numericStringComparer) { "007" };
+Console.WriteLine(set.Contains("7"));
